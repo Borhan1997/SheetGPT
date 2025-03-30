a1cacd474a40ed2161261830c31e6916715f611d
Borhan1997 <96382804+Borhan1997@users.noreply.github.com>
2025-03-30 13:19:26 +0000

    Agent Integration

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+#from langchain.agents import create_csv_agent
+from langchain_experimental.agents import create_csv_agent
+
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+
+def main():
+
+    load_dotenv()
+
+    st.set_page_config(page_title="SheetGPT")
+    st.header("Ask your CSV")
+
+    user_csv = st.file_uploader("Upload your CSV file", type="csv")
+
+    if user_csv is not None:
+        user_question = st.text_input("Ask a question about your CSV")
+
+        llm = OpenAI(temperature=0)
+        agent = create_csv_agent(llm, user_csv, verbose = False, allow_dangerous_code=True)
+
+        if user_question is not None and user_question != "":
+            response = agent.run(user_question)
+            
+            st.write(response)
+            #st.write(f"Your question was : {user_question}")
+
+if __name__ == "__main__":
+    main()
